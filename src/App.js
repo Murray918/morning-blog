ef6c003634a3a6632fbf37791263b5e50f81b096
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-07-03 10:29:13 -0500

    installs/mounts browserrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import Main from './Main'
+import Login from './Login'
 
 
-function App() {
-  return (
-    <div className="container">
-      <Main /> 
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    isLoggedIn : false
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/main" component={Main} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+
