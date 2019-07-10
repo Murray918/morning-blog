022b15fc08cd421ba9d78d1e571e14a179547c4f
Andrew Murray <darbyguitars@gmail.com>
2019-07-10 14:11:54 -0500

    fixing export and prop-types orders

--- src/Login/ProtectedRoute.js
+++ src/Login/ProtectedRoute.js
@@ -3,23 +3,23 @@ import { Route, Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 function ProtectedRoute({ isLoggedIn, component: Component, ...rest }) {
-    return (
-        <Route
-            {...rest}
-            render={ props => (
-                isLoggedIn ?  <Component />
-                :
-                <Redirect 
-                to={{ pathname: "/", state: { from : props.location }}}
-                />
-        
-            )}
-        />
-    )
-}
-
-ProtectedRoute.propTypes = {
-    component: PropTypes.elementType
+	return (
+		<Route
+			{...rest}
+			render={props =>
+				isLoggedIn ? (
+					<Component />
+				) : (
+					<Redirect to={{ pathname: '/', state: { from: props.location } }} />
+				)
+			}
+		/>
+	)
 }
 
 export default ProtectedRoute
+
+ProtectedRoute.propTypes = {
+	component: PropTypes.elementType,
+	isLoggedIn: PropTypes.bool
+}
